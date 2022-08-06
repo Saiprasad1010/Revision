f1c5ca80ccbb474d636858091adb7cd9e18b74d7
Saiprasad <saiprasadpatil1010@gmail.com>
2022-08-06 10:23:13 +0530

    first commit

---
+++ src/RevisionOfClass/DataTypes.java
@@ -0,0 +1,49 @@
+package RevisionOfClass;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hi I am Saiprasad Subhash Patil");
+		System.out.println("I am from Kolhapur");
+		System.out.println("I am Civil Engineer");
+		
+		byte a;
+		a=12;
+		System.out.println("show the numbers of:"+a);
+		
+		int b,c,d,e;
+		b=25;
+		c=65;
+		d=85;
+		e=b*d/c;
+		System.out.println("multiplication and division of three numbers (25*85/65):"+e);
+		
+		short f=5698;
+		System.out.println("Show the number:"+f);
+		
+		float g=365.21f;
+		System.out.println("Show the Decimal number:"+g);
+		
+		double h=12.56984;
+		System.out.println("Show the decimal number:"+h);
+		
+		boolean i=true;
+		System.out.println("Show true/False:"+i);
+		
+		char j='l';
+		System.out.println("show the given chgaracter:"+j);
+		
+		int m,n,o,p;
+		m=78;
+		n=98;
+		o=25;
+		p=m+n-o*(m/n)+(o%m);
+		System.out.println("Solve the given mathematical equation:"+p);
+		
+
+	}
+
+}
